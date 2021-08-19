35cccba75b7ee04b7a9273630db456ac71ff32eb
JohnAra <87151137+joao-arauhjo@users.noreply.github.com>
2021-08-19 19:42:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//Exercício 4 - Elabore um programa que recebe dois valores inteiros e mostra a soma desses valores//
+let valor1 = +prompt("Digite um número inteiro: ");
+let valor2 = +prompt("Digite outro número inteiro: ");
+let resultado = (valor1 + valor2);
+let media = (valor1 + valor2) /2;
+
+let valor3 = console.log("A soma dos valores é: " +resultado+ ", e a média deles é: " +media)
+
